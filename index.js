4c89f3d0fbc399416bdb32264b68166b18665375
unknown <Joseph>
2022-08-13 02:24:52 +0800

    first commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("WORKING!!!");
+  res.send("TESTING HEROKU!!!");
 });
 
 app.listen(process.env.PORT || 5000);
