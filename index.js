1978f6bf46230001a875d51306590275ce6658fd
unknown <Joseph>
2022-08-14 14:46:46 +0800

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 
 const express = require("express");
 const app = express();
-const path = require('path'); 
 
-app.get("test.html", function (req, res) {
-  res.sendFile(path.join(__dirname + '/test.html')); 
+app.get("/", function (req, res) {
+  res.send("TESTING HEROKU!!!");
 });
 
 app.listen(process.env.PORT || 5000);
