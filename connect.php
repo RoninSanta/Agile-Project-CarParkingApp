64d806ce352e9f683030e93f08e81d0177abc9d0
unknown <Joseph>
2022-08-15 13:07:10 +0800

    second commit

--- connect.php
+++ connect.php
@@ -13,11 +13,12 @@
 
 	// Database connection
 	$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-	//$conn = new mysqli('localhost','root','','');
+	//$conn = new mysqli('localhost','root','databasename');
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
 	} else {
+		//$stmt = $conn->prepare("insert into table_name(vehicle,vehType) values(?,?)");
 		$stmt = $conn->prepare("insert into smoove app database(vehicle,vehType) values(?,?)");
 		$stmt->bind_param("ss", $vehicle,$vehType );
 		$execval = $stmt->execute();
